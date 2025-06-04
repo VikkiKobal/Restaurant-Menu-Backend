fbc7773fa7d238ffe82466d76e4d4611f9343c5f
Viktoriia Kobal <kobal.viktoriia@student.uzhnu.edu.ua>
2025-06-04 23:40:46 +0300

    changes in DB

--- db.js
+++ db.js
@@ -3,20 +3,10 @@ const { Pool } = require('pg');
 
 const isRender = process.env.RENDER === 'true';
 
-const poolConfig = process.env.DATABASE_URL
-    ? {
-        connectionString: process.env.DATABASE_URL,
-        ssl: isRender ? { rejectUnauthorized: false } : false,
-    }
-    : {
-        user: process.env.DB_USER,
-        database: process.env.DB_NAME,
-        password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT,
-        ssl: isRender ? { rejectUnauthorized: false } : false,
-    };
-
-const pool = new Pool(poolConfig);
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: isRender ? { rejectUnauthorized: false } : false,
+});
 
 const connect = async () => {
     try {
