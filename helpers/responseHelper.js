1b4d41f837ee3c48b3e540251ea867327a1d39f9
Viktoriia Kobal <161163769+VikkiKobal@users.noreply.github.com>
2025-06-13 13:49:42 +0300

    Delete helpers directory

--- helpers/responseHelper.js
+++
@@ -1,8 +0,0 @@
-exports.success = (res, data) => {
-    res.status(200).json(data);
-};
-
-exports.error = (res, message, err = null) => {
-    console.error(err);
-    res.status(500).json({ message });
-};
